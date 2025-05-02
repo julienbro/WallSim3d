75d175e3d9e4f4a81975da3e0f39bd260aacb108
julienbro <julienbrohez@outlook.com>
2025-05-02 21:22:00 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,10 @@
+import * as THREE from 'three';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
+
+const { jsPDF } = window.jspdf;
+
+// Initialise ta scène, caméra, renderer, etc.
+// const scene = new THREE.Scene();
+// const camera = new THREE.PerspectiveCamera(...);
+// const renderer = new THREE.WebGLRenderer(...);
+// ... reste de ton JS
